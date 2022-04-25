4e2e99cc5f75bd8ef7b431edbf352d38eaba0fc3
keremOzerdem <kerem_riza@hotmail.com>
2022-04-25 20:59:32 +0300

    code blocks are created

--- src/simple/assembly/index.ts
+++ src/simple/assembly/index.ts
@@ -1,26 +1,50 @@
-import { storage, Context } from "near-sdk-as"
+import {
+	context, // visibility into account, contract and blockchain details
+	ContractPromiseBatch, // make asynchronous calls to other contracts and receive callbacks
+	logging, // append to the execution environment log (appears in JS Developer Console when using near-api-js)
+	storage, // append to the execution environment log (appears in JS Developer Console when using near-api-js)
+} from "near-sdk-as";
 
-// return the string 'hello world'
-export function helloWorld(): string {
-  return 'hello world'
+import { AccountId } from "../../utils";
+import { Advertising, allAds } from "./model";
+
+// Health Check
+export function healtCheck(req: string, rep: string): string {
+	storage.setString(req,rep);
+	let result = storage.getString(req);
+	if(result) {
+		return "Going great!";
+	}
+	return "Heart rate of the function is abnormal!";
 }
 
-// read the given key from account (contract) storage
-export function read(key: string): string {
-  if (storage.hasKey(key)) {
-    return `✅ Key [ ${key} ] has value [ ${storage.getString(key)!} ]`
-  } else {
-    return `🚫 Key [ ${key} ] not found in storage. ( ${storageReport()} )`
-  }
+// Create and Publish an Advertising
+export function publishAdvertising(text: string): News {
+	logging.log("A scratch to the history of the Advertising future!");
+	return Advertising.publish(text);
 }
 
-// write the given value at the given key to account (contract) storage
-export function write(key: string, value: string): string {
-  storage.set(key, value)
-  return `✅ Data saved. ( ${storageReport()} )`
+// Take a Look to Archieve
+export function bringAdvertising(id: u32): Advertising {
+	logging.log("You can send your thanks to the Advertisingman using 'sendGratitude' function!");
+	return Advertising.bringFromArchieve(id);
 }
 
-// private helper method used by read() and write() above
-function storageReport(): string {
-  return `storage [ ${Context.storageUsage} bytes ]`
+// Read a Advertising
+export function readAdvertising(): Advertising[] {
+	assert(allAds.length > 0, "There is not a remarkable advertising in these days.");
+	return Advertising.readAnAdvertising();
+}
+
+// Delete your Advertising
+export function deleteAdvertising(id: u32): void {
+	logging.log("Looking forward to your new advertising!");
+	News.deleteAnAdvertising(id);
+}
+
+//Donate the Advertisingman
+export function sendGratitude(advertisingman: AccountId): void{
+	assert(context.accountBalance > context.attachedDeposit, "Your balance is not enough!");
+	logging.log(`Warning! ${context.attachedDeposit.toString()} yoktoNEAR sent`);
+	ContractPromiseBatch.create(advertisingman).transfer(context.attachedDeposit);
 }
